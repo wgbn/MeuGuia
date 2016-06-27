4bc9ed72eadcb116d4483174dac1b01e90110010
Walter Gandarella <jebinha@gmail.com>
2016-06-27 09:30:12 -0300

    finalizado

--- app/src/main/java/br/com/wgbn/meuguia/MainActivity.java
+++ app/src/main/java/br/com/wgbn/meuguia/MainActivity.java
@@ -1,142 +1,210 @@
+/**
+ * Aplicativo desenvolvido como trabalho final da matéria de Desenvolvimento Móvel
+ * com prof. André Portugal
+ *
+ * Este app usa a lib LocationManager para facilitar o acesso à localização atual do usuário:
+ * http://android-arsenal.com/details/1/3148
+ *
+ * Este app usa a lib HttpAgent para facilitar o uso de requisições HTTP abstraindo o AsyncTask
+ * https://android-arsenal.com/details/1/3756
+ *
+ * Este app usa a lib Gson, do Google, para fazer o parser do Json recebido
+ */
+
 package br.com.wgbn.meuguia;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ProgressDialog;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.gson.Gson;
+import com.studioidan.httpagent.HttpAgent;
+import com.studioidan.httpagent.JsonArrayCallback;
+import com.yayandroid.locationmanager.LocationBaseActivity;
+import com.yayandroid.locationmanager.LocationConfiguration;
+import com.yayandroid.locationmanager.constants.FailType;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.LinkedList;
 
 import br.com.wgbn.meuguia.Models.Lugar;
 import br.com.wgbn.meuguia.Utils.LugaresAdapter;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class MainActivity extends LocationBaseActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private ListView lstLugares;
     private ArrayList<Lugar> lugares = new ArrayList<Lugar>();
+    private Location localizacao;
+
+    ProgressDialog progressDialog;
+    String baseUrl = "http://lugares-meuguia.rhcloud.com/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // fragmento que contém o mapa
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.mapa);
         mapFragment.getMapAsync(this);
 
         lstLugares = (ListView) findViewById(R.id.lstLugares);
 
-        this.mock();
-        this.preencheLista();
+        // função da lib LocationManager que pega a localização atual
+        getLocation();
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Localizando");
     }
 
+    /**
+     * Função que é executada quando o widget do Google Maps está pronto
+     * @param googleMap
+     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+    }
 
-        LatLng lauro = new LatLng(-12.8599849, -38.3620983);
-        //mMap.addMarker(new MarkerOptions().position(lauro).title("Lauro de Freitas"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(lauro, 12.0f));
+    /**
+     * Função chamada pelo LocationManager quando consegue a localização do usuário
+     * Está função centraliza o mapa na posição atual do usuário, exibe o diálogo de carregamento
+     * e dispara a consulta ao webservice
+     */
+    private void carregaMapa(){
+        if (this.localizacao != null){
+            LatLng seuLocal = new LatLng(this.localizacao.getLatitude(), this.localizacao.getLongitude());
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(seuLocal, 14.0f));
 
-        for (Lugar lugar : this.lugares) {
-            mMap.addMarker(new MarkerOptions().position(new LatLng(lugar.lat, lugar.lon)).title(lugar.nome));
+            progressDialog.show();
+
+            carregaLugaresProximos();
         }
     }
 
-    private void mock(){
-        Lugar l1 = new Lugar();
-        l1.lat = -12.8599849;
-        l1.lon = -38.3620983;
-        l1.nome = "Lauro de Freitas";
-        l1.historia = "lorem ipsum amet";
-        l1.fundacao = new Date(1930, 06, 14);
-        l1.imagem = "http://imagem.bocaonews.com.br/fotos/noticias/130903/mg/002.jpg?time=1449526113";
+    /**
+     * Esta função usa a lib HTTPAgent para consultar o webservice e recuperar o JSON com os locais
+     * próximos ao usuário. Caso o resultado contenha algum local, a função popula o ArrayList de lugares
+     * e chama o método que preenche a listView e marca os pontos no mapa.
+     */
+    private void carregaLugaresProximos(){
+        HttpAgent.get(this.baseUrl)
+                .queryParams("coords", this.localizacao.getLatitude() + "," + this.localizacao.getLongitude())
+                .goJsonArray(new JsonArrayCallback() {
+                    @Override
+                    protected void onDone(boolean success, JSONArray jsonArray) {
+                        progressDialog.dismiss();
+                        Gson gson;
+                        lugares = new ArrayList<Lugar>();
 
-        this.lugares.add(l1);
+                        try {
+                            for (int i = 0; i < jsonArray.length(); i++){
+                                gson = new Gson();
+                                lugares.add(gson.fromJson(jsonArray.getString(i), Lugar.class));
+                            }
+                        } catch (JSONException e){
+                            e.printStackTrace();
+                        }
 
-        Lugar l2 = new Lugar();
-        l2.lat = -12.881303;
-        l2.lon = -38.3165084;
-        l2.nome = "Hospital Geral Menandro de Farias";
-        l2.historia = "lorem ipsum amet";
-        l2.fundacao = new Date(1930, 06, 14);
-        l2.imagem = "http://imagem.bocaonews.com.br/ckfinder/userfiles/fotos_e_imagens/hospital_menandro_farias_bocaonews_rv%20(8).JPG";
-
-        this.lugares.add(l2);
-
-        l2 = new Lugar();
-        l2.lat = -12.9383436;
-        l2.lon = -38.3892009;
-        l2.nome = "Faculdade SENAI CIMATEC";
-        l2.historia = "lorem ipsum amet";
-        l2.fundacao = new Date(1930, 06, 14);
-        l2.imagem = "https://lh6.googleusercontent.com/-Z_nwLY4VAKU/U7MLToBQoUI/AAAAAAAAABA/0xf2Q9CwXisIG9g5nWZijVjCZQQc-hhOQ/s408-k-no/";
-
-        this.lugares.add(l2);
-
-        l2 = new Lugar();
-        l2.lat = -12.9816632;
-        l2.lon = -38.4666431;
-        l2.nome = "Shopping da Bahia";
-        l2.historia = "lorem ipsum amet";
-        l2.fundacao = new Date(1930, 06, 14);
-        l2.imagem = "https://lh4.googleusercontent.com/proxy/gYvauyTuHx22Iu44DtPRzCUy5DDqu7Un8LObAtETDIgxMLZ4mrXJLk-pJPDHyIKmqCsfyvtI3-tXS_aa4h8e0Xda9-kniQ=w408-h306";
-
-        this.lugares.add(l2);
-
-        l2 = new Lugar();
-        l2.lat = -12.978135;
-        l2.lon = -38.457105;
-        l2.nome = "Salvador Shopping";
-        l2.historia = "lorem ipsum amet";
-        l2.fundacao = new Date(1930, 06, 14);
-        l2.imagem = "https://lh4.googleusercontent.com/proxy/RUzRg_Z81qHQ-gH6vabTqPqLRT8zJ-o3-Y8FiuhUVhTXH3smT1ZBSN7EiW01uO0oVB0PcCM9AOBLvX6F9fsZL8ANE9ne8Rg=w408-h306";
-
-        this.lugares.add(l2);
-
-        l2 = new Lugar();
-        l2.lat = -12.9994458;
-        l2.lon = -38.4747004;
-        l2.nome = "Parque da Cidade";
-        l2.historia = "lorem ipsum amet";
-        l2.fundacao = new Date(1930, 06, 14);
-        l2.imagem = "https://lh6.googleusercontent.com/proxy/j0y28mpx_Y1W1gDr8AqQ7YneaZwxhwVT2M3r_iND3cs6gH1AWZJtcdSDkvQ564WXOp6V64h-nEtwNIkvCq0xg041RcKDww=w408-h306";
-
-        this.lugares.add(l2);
-
-        l2 = new Lugar();
-        l2.lat = -12.9832466;
-        l2.lon = -38.5132682;
-        l2.nome = "Estação da Lapa";
-        l2.historia = "lorem ipsum amet";
-        l2.fundacao = new Date(1930, 06, 14);
-        l2.imagem = "https://lh3.googleusercontent.com/proxy/9LuFsNotux-yvnHk-Gy7P-Il8yA3FL0AMs7Ue4oKL0hTzWAPk9K7BI0cV41S9xcexQw1akcaPHIPIuk429qXAeXAeJg27JMGWv4frbNj2wnjz1g6CXaNhisH3h6b0UWFb2uk3crBGMGXHeuAgAiSgOU6wYzWk0I=w408-h305";
-
-        this.lugares.add(l2);
+                        if (lugares.size() > 0)
+                            preencheLista();
+                    }
+                });
     }
 
+    /**
+     * Esta função preenche a listView com os lugares próximos a partir do ArrayList e marca este lugares no mapa
+     */
     private void preencheLista(){
         LugaresAdapter adapter = new LugaresAdapter(this, lugares);
         lstLugares.setAdapter(adapter);
         lstLugares.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(lugares.get(position).lat, lugares.get(position).lon), 15.0f));
-                //
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(lugares.get(position).coords[1], lugares.get(position).coords[0]), 16.0f));
             }
         });
+
+        if (this.lugares.size() > 0)
+            for (Lugar lugar : this.lugares) {
+                mMap.addMarker(new MarkerOptions().position(new LatLng(lugar.coords[1], lugar.coords[0])).title(lugar.nome));
+            }
+    }
+
+    /**
+     * Função da lib LocationManager que configura a lib e os providers de localização
+     * @return
+     */
+    @Override
+    public LocationConfiguration getLocationConfiguration() {
+        return new LocationConfiguration()
+                .keepTracking(true)
+                .askForGooglePlayServices(true)
+                .setGPSMessage("Você se importaria de ligar a navegação GPS?")
+                .setRationalMessage("Autorize a utilização do GPS");
+    }
+
+    /**
+     * Função da lib LocationManager que prover um fallback para o caso de erro na identificação do
+     * local. Permite que tratemos o erro de geolocalização.
+     * Neste caso apenas informo ou usuário que não foi possível achar o local onde ele está.
+     * @param failType
+     */
+    @Override
+    public void onLocationFailed(int failType) {
+        switch (failType) {
+            case FailType.PERMISSION_DENIED: {
+                Toast.makeText(MainActivity.this, "Não foi possível obter a localização, porque o usuário não deu permissão", Toast.LENGTH_LONG).show();
+                break;
+            }
+            case FailType.GP_SERVICES_NOT_AVAILABLE:
+            case FailType.GP_SERVICES_CONNECTION_FAIL: {
+                Toast.makeText(MainActivity.this, "Não foi possível obter a localização, porque o Google Play Services não está disponível", Toast.LENGTH_LONG).show();
+                break;
+            }
+            case FailType.NETWORK_NOT_AVAILABLE: {
+                Toast.makeText(MainActivity.this, "Não foi possível obter a localização, porque a rede não está acessível", Toast.LENGTH_LONG).show();
+                break;
+            }
+            case FailType.TIMEOUT: {
+                Toast.makeText(MainActivity.this, "Não foi possível obter a localização e tempo de espera", Toast.LENGTH_LONG).show();
+                break;
+            }
+            case FailType.GP_SERVICES_SETTINGS_DENIED: {
+                Toast.makeText(MainActivity.this, "Não foi possível obter a localização, porque o usuário não ativar os provedores via settingsApi", Toast.LENGTH_LONG).show();
+                break;
+            }
+            case FailType.GP_SERVICES_SETTINGS_DIALOG: {
+                Toast.makeText(MainActivity.this, "Não foi possível exibir diálogo settingsApi", Toast.LENGTH_LONG).show();
+                break;
+            }
+        }
+    }
+
+    /**
+     * Função daã lib LocationManager que é executada cada vez que a posição do usuário é alterada
+     * A localizaão é salva num atributo local e a função carregaMapa é chamada para centralizar o
+     * mapa na posição atual
+     * @param location
+     */
+    @Override
+    public void onLocationChanged(Location location) {
+        this.localizacao = location;
+        this.carregaMapa();
     }
 }
--- app/src/main/java/br/com/wgbn/meuguia/Models/Lugar.java
+++ app/src/main/java/br/com/wgbn/meuguia/Models/Lugar.java
@@ -1,55 +1,17 @@
 package br.com.wgbn.meuguia.Models;
 
-import android.os.Parcel;
-import android.os.Parcelable;
+/**
+ * POJO que representa um lugar
+ */
+public class Lugar {
 
-import java.util.Date;
-
-public class Lugar implements Parcelable {
-
-    public Double   lat;
-    public Double   lon;
-    public String   nome;
-    public String   historia;
-    public String   imagem;
-    public Date     fundacao;
+    public String nome;
+    public String texto;
+    public String cidade;
+    public String bairro;
+    public String uf;
+    public String imagem;
+    public Double[] coords;
 
     public Lugar(){ }
-
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeValue(this.lat);
-        dest.writeValue(this.lon);
-        dest.writeString(this.nome);
-        dest.writeString(this.historia);
-        dest.writeString(this.imagem);
-        dest.writeLong(this.fundacao != null ? this.fundacao.getTime() : -1);
-    }
-
-    protected Lugar(Parcel in) {
-        this.lat = (Double) in.readValue(Double.class.getClassLoader());
-        this.lon = (Double) in.readValue(Double.class.getClassLoader());
-        this.nome = in.readString();
-        this.historia = in.readString();
-        this.imagem = in.readString();
-        long tmpFundacao = in.readLong();
-        this.fundacao = tmpFundacao == -1 ? null : new Date(tmpFundacao);
-    }
-
-    public static final Parcelable.Creator<Lugar> CREATOR = new Parcelable.Creator<Lugar>() {
-        @Override
-        public Lugar createFromParcel(Parcel source) {
-            return new Lugar(source);
-        }
-
-        @Override
-        public Lugar[] newArray(int size) {
-            return new Lugar[size];
-        }
-    };
 }
